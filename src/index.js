b00544488be72e30ed4919592d4a31893196849f
George Connor <grgcnnr@gmail.com>
2019-05-08 13:06:18 +1200

    Update tag list to be ties to category relation and not freeform.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { TextInput , Pill} from '@contentful/forma-36-react-components';
 import { init } from 'contentful-ui-extensions-sdk';
+import { CheckboxField, FieldGroup, HelpText } from '@contentful/forma-36-react-components';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
-import Tag from './components/tag'
+
 
 class App extends React.Component {
   static propTypes = {
@@ -18,149 +18,123 @@ class App extends React.Component {
     super(props);
     this.sdk = this.props.sdk;
     this.state = {
-      tags: [],
-      inputValue: '',
-      value: [],
-      suggestedTags: []
+      suggestedTags: [],
+      selectedTagIDs: [],
     };
 
-    this.inputRef = React.createRef()
+    const categoryID = this.sdk.parameters.instance.categoryFieldName || 'category';
 
+    this.categoryField = this.sdk.entry.fields[categoryID];
   }
 
   componentDidMount() {
     this.sdk.window.startAutoResizer();
-    let tags = this.getTags()
-    tags.then( tags => {
-      this.setState({tags: tags})
-    })
+    this.detachCategoryChangeHandler = this.categoryField.onValueChanged(this.onCategoryChanged)
+
+    const intialValue = this.sdk.field.getValue();
+    if (intialValue) {
+      this.setState({selectedTagIDs: intialValue.map( item => item.sys.id )})
+    }
+
     // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
     this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
       this.onExternalChange
     );
 
-    this.inputRef.current.addEventListener('keydown', this.onInputKeyDown);
   }
 
   componentWillUnmount() {
     if (this.detachExternalChangeHandler) {
       this.detachExternalChangeHandler();
     }
-    this.inputRef.current.removeEventListener('keydown', this.onInputKeyDown);
+    if (this.detachCategoryChangeHandler) {
+      this.detachCategoryChangeHandler();
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState, snapshot){
+    //gonna assume react is faster than a user at clicking checkboxes
+    if (prevState.selectedTagIDs.length !== this.state.selectedTagIDs.length) {
+
+      this.props.sdk.field.setValue(
+        this.state.selectedTagIDs.map(tagID => {
+          return {
+            sys: {
+              type: "Link",
+              linkType: "Entry",
+              id: tagID,
+            }
+          }
+        }
+      ));
+    }
   }
 
   onExternalChange = value => {
-    this.setState({ value });
+    if (value) {
+      this.setState({selectedTagIDs: value.map( item => item.sys.id )})
+    } else{
+      this.setState({selectedTagIDs: []})
+    }
   };
+
+  onCategoryChanged = async value => {
+    // If a category is set
+    if(this.categoryField.getValue()){
+      const categoryID = this.categoryField.getValue().sys.id;
+
+      // get the new category from the link id
+      const category = await this.sdk.space.getEntry(categoryID);
+      const tagIDs = category.fields.tags['en-US'].map(tag => tag.sys.id);
+
+      // Get tags that are linked to those categories
+      const tags = await this.sdk.space.getEntries({
+        content_type: 'tag',
+        'sys.id[in]': tagIDs.join(',')
+      });
+
+      this.setState({'suggestedTags': tags.items})
+    }
+
+    else{
+      this.setState({
+        'suggestedTags': [],
+        selectedTagIDs: []
+      })
+    }
+
+  }
 
 
   onInputChange = e => {
     const value = e.currentTarget.value
-    this.setState({ inputValue: value })
 
-
-    if (value == ''){
-      this.setState({suggestedTags: []})
+    if (this.state.selectedTagIDs.indexOf(value) > -1){
+      this.setState({selectedTagIDs: this.state.selectedTagIDs.filter(id => id !== value )})
     } else{
-      const matcher = new RegExp('^' + value);
-      let candidates = this.state.tags.filter(tag => {
-        return matcher.test(tag) && this.state.value.indexOf(tag) === -1;
-      })
-
-      this.setState({suggestedTags: candidates})
-    }
-  }
-
-  onInputKeyDown = e => {
-    const key = event.which || event.keyCode;
-    if ((key === 39 || key === 9) && this.state.suggestedTags.length) { // 39 is right arrow, 9 is tab
-      event.preventDefault();
-      this.setState({inputValue: this.state.suggestedTags[0]})
+      this.setState({selectedTagIDs: [...this.state.selectedTagIDs, value]})
     }
 
-    if (key === 13  && this.state.inputValue.length){
-      if(this.sdk.parameters.instance.allowAdd || this.state.tags.indexOf(this.state.inputValue) > -1 ) {
-        this.addTag(this.state.inputValue)
-      } else{
-        this.setState({inputValue: this.state.suggestedTags[0]})
-      }
-    }
-  }
-
-  resetInput = () => {
-    this.setState({
-      inputValue: '',
-      suggestedTags: []
-    })
-    this.inputRef.current.focus()
-
-  }
-
-  addTag = tagVal => {
-    let newTags = this.state.value.slice(0)
-    newTags.push(tagVal)
-    this.props.sdk.field.setValue(newTags);
-    this.resetInput()
-  }
-
-  removeTag = tagVal => {
-    let newTags = this.state.value.slice(0)
-    let idx = newTags.indexOf(tagVal)
-    if (idx > -1){
-      newTags.splice(idx, 1)
-    }
-    this.props.sdk.field.setValue(newTags);
-  }
-
-  getTags = () => {
-    return new Promise((resolve, reject) =>{
-      const searchField = this.sdk.parameters.instance.fieldName || 'tags'
-      const field = this.sdk.field
-
-      // Get all entries and find field that matches
-      this.sdk.space.getEntries().then(entries => {
-        let tags = [];
-        entries.items.forEach(item => {
-          if (item.fields.hasOwnProperty(searchField) && item.fields[searchField].hasOwnProperty(field.locale) && item.fields[searchField][field.locale].forEach ) {
-            item.fields[searchField][field.locale].forEach(function(tag) {
-              if ( tags.indexOf(tag) == -1 ){
-                tags.push(tag)
-              }
-            });
-          }
-        })
-        resolve(tags);
-      }).catch(() => reject() );
-
-    });
   }
 
   render() {
     return (
-        <div>
-          <TextInput
-            width="large"
-            type="text"
-            id="my-field"
-            autoComplete="off"
-            value={this.state.inputValue}
-            onChange={this.onInputChange}
-            inputRef={this.inputRef}
-          />
 
-          <div>
-          {this.state.suggestedTags && this.state.suggestedTags.map((tag, i) => (
-              <Tag onClick={() => this.addTag(tag)} key={tag} primary={i === 0}>{tag}</Tag>
-          ))}
-          </div>
-          {this.state.value && this.state.value.map((tag) => (
-            <div key={tag}  className="pillContainer">
-             <Pill onClose={() => this.removeTag(tag)} label={tag} />
-            </div>
-          ))}
-
-        </div>
+        this.state.suggestedTags.length ?
+          <FieldGroup>
+            {this.state.suggestedTags.map(tag => (
+                <CheckboxField
+                    onChange={this.onInputChange}
+                    key={tag.sys.id}
+                    name={tag.fields.title['en-US']}
+                    value={tag.sys.id}
+                    labelText={tag.fields.title['en-US']}
+                    id={tag.sys.id}
+                    checked={this.state.selectedTagIDs.indexOf(tag.sys.id) > -1}
+                />
+            ))}
+          </FieldGroup> :
+          <HelpText>Waiting for category....</HelpText>
     );
   }
 }
